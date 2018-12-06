a35aaf664be543a6dc51f52f6815583026e2a8f1
idris <id.gasmi@laposte.net>
2018-12-07 00:34:54 +0100

    une_bonne_offre
    description

---
+++ my-app/src/app/mock-offers.ts
@@ -0,0 +1,9 @@
+import {Offer} from "./offer";//une offre dans offers lol
+
+export const OFFERS : Offer[]=[//une offre dans offers lol
+    {id : 1, title : "Pret étudiant", description:"Il est super bien1" },//une offre dans offers lol
+    {id : 2, title : "Pret travailleur", description:"Il est super bien2" },//une offre dans offers lol
+    {id : 3, title : "Pret vieux", description:"Il est super bien3" },//une offre dans offers lol
+    {id : 4, title : "Pret spécial Sahara", description:"Il est super bien3.1tatata" }//une offre dans offers lol
+//at this moment La chaleur du desert nous a fait perdre notre developpeur de commentaire; commit.
+];
